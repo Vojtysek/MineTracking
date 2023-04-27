699fa407c9561a50daed6f5f4980c55f95bbc09d
Vojtěch Döme <ojta@post.cz>
2023-04-27 12:05:26 +0200

    Project setup

---
+++ src/main/java/me/vojtysek/minetracking/MineTracking.kt
@@ -0,0 +1,15 @@
+package me.vojtysek.minetracking
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MineTracking : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+
+
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
